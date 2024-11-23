10d5fc961dba46e1ddb5d61211cd55d8c26d7f4f
DSRN0000 <dsrn0000@gmail.com>
2024-11-23 14:54:41 +0900

    GetMappring

--- src/main/java/com/ll/chatting/domian/chat/chatRoom/controller/ApiV1ChatControllers.java
+++ src/main/java/com/ll/chatting/domian/chat/chatRoom/controller/ApiV1ChatControllers.java
@@ -3,12 +3,14 @@ package com.ll.chatting.domian.chat.chatRoom.controller;
 
 import com.ll.chatting.domian.chat.chatRoom.entity.ChatRoom;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController // 창구역할
 @RequestMapping("/api/v1/chat/rooms")
@@ -32,8 +34,23 @@ public class ApiV1ChatControllers {
         );
     }};
 
+
+
     @GetMapping
     public List<ChatRoom> getChatRooms() {
         return chatRooms;
+    }
+
+    @GetMapping("/{id}")
+    public ChatRoom getChatRoom(
+            @PathVariable long id
+    ) {
+        Optional<ChatRoom> opChatRoom = findById(id);
+
+        return opChatRoom.get();
+    }
+
+    private Optional<ChatRoom> findById(long id) {
+        return  chatRooms.stream().filter(chatRoom -> chatRoom.getId() == id).findFirst();
     }
 }
